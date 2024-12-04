02360c48b64f5e04c4cd4451ad324fed1098b824
marcelo13oliveira <83042641+marcelo13oliveira@users.noreply.github.com>
2024-12-04 08:54:00 -0300

    Calculadora

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numero1;
+        int numero2;
+        int soma;
+        int operacao = Integer.parseInt(JOptionPane.showInputDialog("Escolha a operação: \n1 - Soma: \n2 - Subtração: \n3 - Divisão: \n4 - Multiplicação: "));
+        switch (operacao) {
+            case 1:
+                numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número:"));
+                numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número:"));
+                soma = numero1 + numero2;
+                System.out.println("Resultado: " + soma);
+                break;
+            case 2:
+                numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número:"));
+                numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número:"));
+                soma = numero1 - numero2;
+                System.out.println("Resultado: " + soma);
+                break;
+            case 3:
+                numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número:"));
+                numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número:"));
+                soma = numero1 / numero2;
+                System.out.println("Resultado: " + soma);
+                break;
+            case 4:
+                numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro número:"));
+                numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo número:"));
+                soma = numero1 * numero2;
+                System.out.println("Resultado: " + soma);
+                break;
+        }
+
+    }
+
+}
+
